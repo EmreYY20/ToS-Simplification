fdd73b3f15ce485532de501dcd55ef3a20e7a0fd
EmreYY20 <120115560+EmreYY20@users.noreply.github.com>
2023-12-12 21:30:24 +0100

    fine-tuning update

--- Modeling/models/abstractive_model.py
+++ Modeling/models/abstractive_model.py
@@ -1,53 +1,75 @@
 import torch
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import BartTokenizer, BartForConditionalGeneration, Trainer, TrainingArguments, DataCollatorForSeq2Seq
 from sklearn.model_selection import train_test_split
-from transformers import Trainer, TrainingArguments, DataCollatorForSeq2Seq
 import pandas as pd
+from torch.utils.data import Dataset
 
-# Load the fine-tuning dataset
-df = pd.read_json('dataset\dataset.json', lines=True)
+class CustomDataset(Dataset):
+    def __init__(self, tokenizer, df, max_length=512, max_target_length=128):
+        self.input_ids = []
+        self.attention_mask = []
+        self.labels = []
 
-# Split the data into training and validation sets
-train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
+        for _, row in df.iterrows():
+            inputs = tokenizer.encode_plus(row['plain_text'], max_length=max_length, return_tensors='pt', truncation=True)
+            labels = tokenizer.encode_plus(row['summary'], max_length=max_target_length, return_tensors='pt', truncation=True)
 
-# Load BART pre-trained model and tokenizer
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+            self.input_ids.append(inputs.input_ids.flatten())
+            self.attention_mask.append(inputs.attention_mask.flatten())
+            self.labels.append(labels.input_ids.flatten())
 
-# Tokenize the input text and summaries
-def tokenize_data(df):
-    inputs = tokenizer(df['plain_text'].tolist(), padding=True, truncation=True, return_tensors='pt')
-    labels = tokenizer(df['summary'].tolist(), padding=True, truncation=True, return_tensors='pt')
-    return inputs, labels
+    def __len__(self):
+        return len(self.input_ids)
 
-train_inputs, train_labels = tokenize_data(train_df)
-val_inputs, val_labels = tokenize_data(val_df)
+    def __getitem__(self, idx):
+        return {'input_ids': self.input_ids[idx], 
+                'attention_mask': self.attention_mask[idx], 
+                'labels': self.labels[idx]}
 
-# Define training configurations
-training_args = TrainingArguments(
-    output_dir='./results',
-    overwrite_output_dir=True,
-    num_train_epochs=3,
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    logging_steps=100,
-    save_steps=500,
-    warmup_steps=500,
-    weight_decay=0.01,
-    logging_dir='./logs',
-    evaluation_strategy="epoch"
-)
+try:
+    # Load the fine-tuning dataset
+    df = pd.read_json('Modeling\models\dataset\dataset.json', lines=True)
 
-# Define the Trainer object
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=DataCollatorForSeq2Seq.from_tensors(train_inputs, train_labels),
-    eval_dataset=DataCollatorForSeq2Seq.from_tensors(val_inputs, val_labels)
-)
+    # Split the data into training and validation sets
+    train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
 
-# Start training using the Trainer instance
-trainer.train()
+    # Load BART pre-trained model and tokenizer
+    tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+    model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 
+    # Create datasets
+    train_dataset = CustomDataset(tokenizer, train_df)
+    val_dataset = CustomDataset(tokenizer, val_df)
 
+    # Define training configurations
+    training_args = TrainingArguments(
+        output_dir='./results',
+        overwrite_output_dir=True,
+        num_train_epochs=3,
+        per_device_train_batch_size=2,
+        per_device_eval_batch_size=2,
+        logging_steps=100,
+        save_steps=500,
+        warmup_steps=500,
+        weight_decay=0.01,
+        logging_dir='./logs',
+        evaluation_strategy="epoch"
+    )
 
+    # Define data collator
+    data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+    # Define the Trainer object
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        data_collator=data_collator
+    )
+
+    # Start training using the Trainer instance
+    trainer.train()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
